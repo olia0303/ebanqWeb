01cb20c2c800afe38c1d5331a0630c966900f7d2
Aliaksandr.Yakavenka <alexandr107yakovenko@gmail.com>
2020-03-10 14:07:02 +0300

    available balance validation was added to request test

--- src/test/java/com/ebanq/web/pageobjects/reports/ReportsPageUser.java
+++ src/test/java/com/ebanq/web/pageobjects/reports/ReportsPageUser.java
@@ -4,15 +4,22 @@ import com.codeborne.selenide.Condition;
 import com.ebanq.web.elements.EbanqAccountSelect;
 import com.ebanq.web.elements.EbanqTable;
 import com.ebanq.web.pageobjects.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.HashMap;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static com.ebanq.web.other.Urls.REPORTS_PAGE;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class ReportsPageUser extends BasePage {
     private final String ACTIVE_REPORTS_PAGE_USER_CSS = "app-specific-account-statement > .container";
+    private final String ACCOUNT_BALANCE_XPATH = "//*[contains(text(), 'Account')]/parent::div/*[contains(@class, 'bottom-hint')]";
+    private final String CURRENT_ACCOUNT_BALANCE_XPATH = "//*[contains(text(), 'Current balance')]/parent::div/*[contains(@class, 'inform-text')]";
 
     @Override
     public ReportsPageUser isPageOpened() {
@@ -28,7 +35,18 @@ public class ReportsPageUser extends BasePage {
     public ReportsPageUser changeAccount(String accountNumber) {
         new EbanqAccountSelect("Account")
                 .selectValue(accountNumber);
+        (new WebDriverWait(getWebDriver(), 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                String accountBalance = $(By.xpath(ACCOUNT_BALANCE_XPATH)).getText();
+                String currentAccountBalance = $(By.xpath(CURRENT_ACCOUNT_BALANCE_XPATH)).getText();
+                return accountBalance.contains(".") && currentAccountBalance.contains(accountBalance);
+            }
+        });
         return this;
+    }
+
+    public String getAccountBalance() {
+        return $(By.xpath(ACCOUNT_BALANCE_XPATH)).getText().replaceAll("[^.\\-\\d]", "");
     }
 
     public ReportsPageUser validateTransferDetails(String ID, String descriptionAccountNumber, String transferAmount) {
--- src/test/java/com/ebanq/web/steps/reports/ReportsUserSteps.java
+++ src/test/java/com/ebanq/web/steps/reports/ReportsUserSteps.java
@@ -3,12 +3,35 @@ package com.ebanq.web.steps.reports;
 import com.ebanq.web.model.Transfer;
 import com.ebanq.web.pageobjects.reports.ReportsPageUser;
 import io.qameta.allure.Step;
+import static org.testng.Assert.assertEquals;
 
 public class ReportsUserSteps {
     private ReportsPageUser reportsPageUser;
+    private double availableDebitBalance;
+    private double updatedAvailableDebitBalance;
 
     public ReportsUserSteps() {
         reportsPageUser = new ReportsPageUser();
+    }
+
+    @Step("Get debit account amount")
+    public ReportsUserSteps getAccountAmount(Transfer transfer) {
+        reportsPageUser.openPage()
+                .isPageOpened()
+                .changeAccount(transfer.getDebitAccountNumber());
+        availableDebitBalance = Double.parseDouble(reportsPageUser.getAccountBalance());
+        return this;
+    }
+
+    @Step("Validate the debit balance received is equal to the calculated balance")
+    public ReportsUserSteps validateFinalDebitAccountAmount(Transfer transfer) {
+        reportsPageUser.openPage()
+                .isPageOpened()
+                .changeAccount(transfer.getDebitAccountNumber());
+        updatedAvailableDebitBalance = Double.parseDouble(reportsPageUser.getAccountBalance());
+        double calculatedAvailableDebitBalance = availableDebitBalance - transfer.getTransferAmount();
+        assertEquals(updatedAvailableDebitBalance, calculatedAvailableDebitBalance, 0.001);
+        return this;
     }
 
     @Step("Validate debit transfer details are correct on Reports Page on the user side")
